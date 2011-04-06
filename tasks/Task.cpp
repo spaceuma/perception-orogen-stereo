24238bf30efeb160e5e6d163b73d4adb0b9edc65
Jan Frädrich <jan.fraedrich@dfki.de>
2011-04-06 16:19:06 +0200

    initial commit

---
+++ tasks/Task.cpp
@@ -0,0 +1,109 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "Task.hpp"
+
+using namespace dense_stereo;
+
+Task::Task(std::string const& name, TaskCore::TaskState initial_state)
+    : TaskBase(name, initial_state)
+{
+}
+
+Task::~Task()
+{
+}
+
+//RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> leftFrame, rightFrame;//besser?
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See Task.hpp for more detailed
+// documentation about them.
+
+bool Task::configureHook()
+{
+    //initialize dense stereo
+    dense_stereo = new DenseStereo();
+  
+    //configuration?
+    
+    if (! TaskBase::configureHook())
+        return false;
+    return true;
+}
+// bool Task::startHook()
+// {
+//     if (! TaskBase::startHook())
+//         return false;    
+//     return true;
+// }
+void Task::updateHook()
+{
+    TaskBase::updateHook();
+    
+    base::samples::frame::Frame leftFrame, rightFrame, leftDisparityFrame, rightDisparityFrame;
+    cv::Mat leftCvFrame, rightCvFrame, leftCvDisparityFrame, rightCvDisparityFrame;
+    
+    if(_left_frame.read(leftFrame) == RTT::NewData && _right_frame.read(rightFrame) == RTT::NewData)
+    {
+      //check if something is connected to the outputports otherwise do not calculate anything
+      if(_left_disparity_frame.connected() || _right_disparity_frame.connected())
+      {
+	//rotate frames by 180 deg and switch them
+	//rightCvFrame = dense_stereo->rotateImage(leftFrame.convertToCvMat(), 180.);
+	//leftCvFrame = dense_stereo->rotateImage(rightFrame.convertToCvMat(), 180.);
+	
+	//flip images is faster by one magnitude then rotation, but recalibration is needed
+	cv::flip(leftFrame.convertToCvMat(),rightCvFrame,-1);
+	cv::flip(rightFrame.convertToCvMat(),leftCvFrame,-1);
+	
+	//for wide angle lens flip is not needed
+	//rightCvFrame = leftFrame.convertToCvMat();
+	//leftCvFrame = rightFrame.convertToCvMat();
+	
+	//calculate the disparities
+	dense_stereo->process_FramePair(leftCvFrame, rightCvFrame, leftCvDisparityFrame, rightCvDisparityFrame);
+	
+	//save input images to filesystem
+	ostringstream left_file_time, right_file_time;
+	left_file_time << leftFrame.time.toMilliseconds();
+	right_file_time << rightFrame.time.toMilliseconds();
+	
+	cv::imwrite("right_frame_" + right_file_time.str() + ".png",rightCvFrame);
+	cv::imwrite("left_frame_" + left_file_time.str() + ".png",leftCvFrame);
+	
+	//test if output images are ok
+	cv::imwrite("right_frame_disp_" + right_file_time.str() + ".png",rightCvDisparityFrame);
+	cv::imwrite("left_frame_disp_" + left_file_time.str() + ".png",leftCvDisparityFrame);
+	
+	//leftDisparityFrame convert to Frame
+	leftDisparityFrame.init(leftCvDisparityFrame.size().width, leftCvDisparityFrame.size().height, leftCvDisparityFrame.elemSize1(), base::samples::frame::MODE_GRAYSCALE);
+	leftDisparityFrame.setImage((const char *)leftCvDisparityFrame.data, leftCvDisparityFrame.size().width * leftCvDisparityFrame.size().height);
+	//leftDisparityFrame convert to Frame
+	rightDisparityFrame.init(rightCvDisparityFrame.size().width, rightCvDisparityFrame.size().height, rightCvDisparityFrame.elemSize1(), base::samples::frame::MODE_GRAYSCALE);
+	rightDisparityFrame.setImage((const char *)rightCvDisparityFrame.data, rightCvDisparityFrame.size().width * rightCvDisparityFrame.size().height);
+
+	//set the frame's timestamp
+	leftDisparityFrame.time = base::Time::now();
+	rightDisparityFrame.time = base::Time::now();
+	
+	//write to outputs
+	_left_disparity_frame.write(leftDisparityFrame);
+	_right_disparity_frame.write(rightDisparityFrame);
+      }
+    }
+}
+// void Task::errorHook()
+// {
+//     TaskBase::errorHook();
+// }
+// void Task::stopHook()
+// {
+//     TaskBase::stopHook();
+// }
+void Task::cleanupHook()
+{
+    TaskBase::cleanupHook();
+
+    delete dense_stereo;
+}
+
---
+++ tasks/Task.hpp
@@ -0,0 +1,87 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.hpp */
+
+#ifndef DENSE_STEREO_TASK_TASK_HPP
+#define DENSE_STEREO_TASK_TASK_HPP
+
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+
+#include <dense_stereo/densestereo.h>
+#include <base/samples/frame.h>
+
+#include "dense_stereo/TaskBase.hpp"
+
+namespace dense_stereo {
+    class Task : public TaskBase
+    {
+	friend class TaskBase;
+    protected:
+	///Instance of dense stereo processing
+	DenseStereo *dense_stereo;
+
+    public:
+        Task(std::string const& name = "dense_stereo::Task", TaskCore::TaskState initial_state = Stopped);
+
+	~Task();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from PreOperational to Stopped. If it returns false, then the
+         * component will stay in PreOperational. Otherwise, it goes into
+         * Stopped.
+         *
+         * It is meaningful only if the #needs_configuration has been specified
+         * in the task context definition with (for example):
+         *
+         *   task_context "TaskName" do
+         *     needs_configuration
+         *     ...
+         *   end
+         */
+        bool configureHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Stopped to Running. If it returns false, then the component will
+         * stay in Stopped. Otherwise, it goes into Running and updateHook()
+         * will be called.
+         */
+        // bool startHook();
+
+        /** This hook is called by Orocos when the component is in the Running
+         * state, at each activity step. Here, the activity gives the "ticks"
+         * when the hook should be called.
+         *
+         * The error(), exception() and fatal() calls, when called in this hook,
+         * allow to get into the associated RunTimeError, Exception and
+         * FatalError states. 
+         *
+         * In the first case, updateHook() is still called, and recover() allows
+         * you to go back into the Running state.  In the second case, the
+         * errorHook() will be called instead of updateHook(). In Exception, the
+         * component is stopped and recover() needs to be called before starting
+         * it again. Finally, FatalError cannot be recovered.
+         */
+        void updateHook();
+
+        /** This hook is called by Orocos when the component is in the
+         * RunTimeError state, at each activity step. See the discussion in
+         * updateHook() about triggering options.
+         *
+         * Call recovered() to go back in the Runtime state.
+         */
+        // void errorHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Running to Stopped after stop() has been called.
+         */
+        // void stopHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Stopped to PreOperational, requiring the call to configureHook()
+         * before calling start() again.
+         */
+        void cleanupHook();
+    };
+}
+
+#endif
+
