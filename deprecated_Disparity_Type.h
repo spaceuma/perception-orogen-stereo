8e990727c5fb87238e570125399cf8ceebb77f72
Christopher Gaudig <christopher.gaudig@dfki.de>
2011-05-18 16:30:38 +0200

    made Disparity_Type.h deprecated

--- deprecated_Disparity_Type.h
+++ deprecated_Disparity_Type.h
@@ -2,18 +2,20 @@
 #define DENSE_DISPARITY_TYPES__H
 
 #include <base/time.h>
+#include <vector>
 
 namespace dense_stereo{
+
   struct disparity_image
   {
-    float* data;
-    
+    std::vector<float> data;
+
     int height;
     int width;
-    
+
     base::Time time;
   };
-  
+
 }
 
 #endif
