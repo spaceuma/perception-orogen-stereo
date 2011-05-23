f0b84eb87ad7342e0b9516f2cbe30fa66e8cd1c4
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-05-23 09:22:20 +0200

    moved calibration config into separate file

--- scripts/log_to_dense_to_png.rb
+++ scripts/log_to_dense_to_png.rb
@@ -9,8 +9,8 @@ include Orocos
   initial_offset = 0
   last_offset = 0
   
-  calibration = 1 #for wide lens calibration
-  #calibration = 0 #for normal lens calibration
+  calibration = :wide #for wide lens calibration
+  #calibration = :normal #for normal lens calibration
 
 if !ARGV.empty?
   log_files = ARGV
@@ -41,86 +41,13 @@ log_files.each_with_index do |log_file,index|
 
     reader = dense_stereo.distance_frame.reader(:type => :buffer, :size => 1)
     counter = 0
-    
-    #configure dense_stereo
-    if(calibration == 1)
-	puts "wide calibration used"
-      #asguard wide angle lens
-      stereoCamCal = dense_stereo.stereoCameraCalibration
-      camLeft = stereoCamCal.CamLeft
-      #intrinsic parameters
-      camLeft.fx = 284.24382
-      camLeft.fy = 285.20982
-      camLeft.cx = 323.63161
-      camLeft.cy = 232.15181
-      camLeft.d0 = 0.00343
-      camLeft.d1 = 0.00094
-      camLeft.d2 = -0.00069
-      camLeft.d3 = 0.00145
-      stereoCamCal.CamLeft = camLeft
-      
-      camRight = stereoCamCal.CamRight
-      camRight.fx = 285.57255
-      camRight.fy = 286.28520
-      camRight.cx = 318.59641
-      camRight.cy = 230.64484
-      camRight.d0 = 0.00112
-      camRight.d1 = 0.00244
-      camRight.d2 = 0.00007
-      camRight.d3 = 0.00010
-      stereoCamCal.CamRight = camRight
-      
-      extrinsic = stereoCamCal.extrinsic
-      extrinsic.tx = -251.92827
-      extrinsic.ty = -0.12599
-      extrinsic.tz = -8.36759
-      
-      extrinsic.rx = -0.00462
-      extrinsic.ry = -0.01025
-      extrinsic.rz = 0.00650
-      stereoCamCal.extrinsic = extrinsic
-      
-      dense_stereo.stereoCameraCalibration = stereoCamCal
-    else
-	puts "normal calibration used"
-      #asguard cam guppy mar 2011
-      stereoCamCal = dense_stereo.stereoCameraCalibration
-      camLeft = stereoCamCal.CamLeft
-      #intrinsic parameters
-      camLeft.fx = 701.60321
-      camLeft.fy = 703.61811
-      camLeft.cx = 324.00043
-      camLeft.cy = 256.81324
-      camLeft.d0 = -0.03207
-      camLeft.d1 = 0.05658
-      camLeft.d2 = -0.00060
-      camLeft.d3 = 0.00207
-      stereoCamCal.CamLeft = camLeft
-      
-      camRight = stereoCamCal.CamRight
-      camRight.fx = 701.48689
-      camRight.fy = 703.66743
-      camRight.cx = 317.56601
-      camRight.cy = 235.52183
-      camRight.d0 = -0.04774
-      camRight.d1 = 0.08554
-      camRight.d2 = -0.00080
-      camRight.d3 = -0.00071
-      stereoCamCal.CamRight = camRight
-      
-      extrinsic = stereoCamCal.extrinsic
-      extrinsic.tx = -253.28725
-      extrinsic.ty = -0.51154
-      extrinsic.tz = -3.52484
-      
-      extrinsic.rx = -0.00202
-      extrinsic.ry = -0.00352
-      extrinsic.rz = 0.00712
-      stereoCamCal.extrinsic = extrinsic
-      
-      dense_stereo.stereoCameraCalibration = stereoCamCal
+
+    # configure the camera calibration
+    dense_stereo.stereoCameraCalibration do |stereoCamCal|
+	cal_config( calibration, stereoCamCal )
     end
     
+    # configure libElas
     libElas_conf = dense_stereo.libElas_conf
     libElas_conf.postprocess_only_left = true
     dense_stereo.libElas_conf = libElas_conf
